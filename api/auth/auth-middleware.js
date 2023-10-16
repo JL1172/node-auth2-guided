bc67edc65a3ed4cf9d3737266f63f6f5434cae60
Jacob Lang <jacoblang72@comcast.net>
2023-10-16 11:01:17 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,11 +1,31 @@
-// AUTHENTICATION
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config");
+
+// AUTHENTICATION //identity
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token,JWT_SECRET,(err,decodedToken)=> {
+      if (err) {
+        next({status : 401, message : `token ba: ${err.message}`}) //too much info for prod
+      } else {
+        req.decodedJwt = decodedToken,
+        console.log(req.decodedJwt)
+        next();
+      }
+    })
+  } else {
+    next({status : 401, message : "what? no token?" })
+  }
 }
 
-// AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
+// AUTHORIZATION //permissions
+const checkRole = role => (req, res, next) => {
+  if (req.decodedJwt && req.decodedJwt.role === role) {
+    next();
+  } else {
+    next({ status : 403, message : "bad bad authorization"})
+  }
 }
 
 module.exports = {
